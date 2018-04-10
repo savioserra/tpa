5a9d17fb6f4a778e7ef8e2d8b43a20f94bc29be7
Sávio Serra <serra.santos.s@gmail.com>
2018-04-10 16:26:29 -0300

    Correção de código (#5)
    * Code smell fix

--- src/main/java/dictionary/EngineFactory.java
+++ src/main/java/dictionary/EngineFactory.java
@@ -3,6 +3,9 @@ package dictionary;
 import dictionary.engines.DefaultEngine;
 
 public class EngineFactory {
+    private EngineFactory() {
+    }
+
     public static <T> HashEngine<T> createDefault() {
         return new DefaultEngine<>();
     }
